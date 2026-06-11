9c608b3665104c7086fc66c8b67ee3b9be3c9a2a
Vitālijs Korabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2026-06-11 19:07:10 +0300

    darbs ar API

--- src/pages/Lietotāja_kartiņa.jsx
+++ src/pages/Lietotāja_kartiņa.jsx
@@ -1,13 +1,26 @@
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Container, Spinner } from "react-bootstrap"
 
 function Lietotāja_kartiņa() {
-    const {userId} = useParams()
+     const {userId} = useParams()
+
+    const [userData, setUserData] = useState({})
+    const [loading, setLoading] = useState(true)
+    
+    useEffect(()=>{
+            fetch(`https://dummyjson.com/users/${userId}`)
+            .then(res => res.json())
+            .then(data=>setUserData(data))
+            .then(setLoading(false));
+    },[])
 
 
     return ( <>
-        <h1>Lietotāja_kartiņa</h1>
-        <p>Informācija par lietotāju: {userId}</p>
-
+        <h1>{userData.firstName} {userData.lastName}</h1>
+        <img src={userData.image} />
+        <p>Vecums: {userData.age}</p>
+        <p>Epasts: {userData.email}</p>
     </> );
 }
 
--- src/pages/Lietotāju_saraksts.jsx
+++ src/pages/Lietotāju_saraksts.jsx
@@ -1,8 +1,53 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom"
+import { Container, Spinner } from "react-bootstrap"
+
 function Lietotāju_saraksts() {
-    return ( 
-    <>
-        <h1>Lietotāju saraksts</h1>
-    </> );
+    // Stavokļi
+    const [usersList, setUsersList] = useState([])
+    const [loading, setLoading] = useState(true)
+
+
+    useEffect(() => {
+        // Fetch vaicājums
+        fetch('https://dummyjson.com/users')
+            .then(res => res.json())
+            .then(data => setUsersList(data.users))
+            .then(setLoading(false))
+
+    }, [])
+    return (
+        <>
+            <h1>Lietotāju saraksts</h1>
+            {loading == true ? (
+                <Spinner animation="border" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                </Spinner>
+            ) : (
+                <Container>
+                    <table className="table table-bordered">
+                        <tbody>
+                            {usersList.map((user, i) => (
+                                <>
+                                    <tr key={i}>
+                                        <td key={`${i}_1`}>{user.firstName}</td>
+                                        <td key={`${i}_2`}>{user.lastName}</td>
+                                        <td key={`${i}_3`}>{user.age}</td>
+                                        <td key={`${i}_4`}>{user.email}</td>
+                                        <td key={`${i}_5`}>{user.phone}</td>
+                                        <td key={`${i}_6`}>
+                                            <Link to={`/user/${user.id}`} className="btn btn-outline-primary">Rediģēt</Link>
+                                        </td>
+                                    </tr>
+
+                                </>
+                            ))}
+                        </tbody>
+                    </table>
+                </Container>
+            )}
+
+        </>);
 }
 
 export default Lietotāju_saraksts;
